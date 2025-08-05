f460a3b4e26f1c98a6270f66378ceff812e7c0b0
Raúl Almeida <personal@raulalmeida.com>
2025-08-05 21:25:50 +0200

    feat: Demonstrate Tailwind CSS workflow by styling the title

--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main class="container mx-auto my-8">
-    <h1 class="text-4xl font-bold text-center">Hello, WordPress with Tailwind!</h1>
+    <h1 class="text-4xl font-bold text-center text-blue-600 underline decoration-wavy">Hello, WordPress with Tailwind!</h1>
     <p class="text-center mt-4">Edit this file in <code>wp-content/themes/my-theme/index.php</code></p>
 </main>
 

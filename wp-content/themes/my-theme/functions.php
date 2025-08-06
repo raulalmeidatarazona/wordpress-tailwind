f448295689df6cff7ad69fa0eceb07554b855064
Raúl Almeida <personal@raulalmeida.com>
2025-08-06 14:15:08 +0200

    use tailwindcss cli

--- wp-content/themes/my-theme/functions.php
+++ wp-content/themes/my-theme/functions.php
@@ -4,4 +4,3 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 'my-theme-style', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.0', 'all' );
 }
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
-?>
--- wp-content/themes/my-theme/index.php
+++ wp-content/themes/my-theme/index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 
 <main class="container mx-auto my-8">
-    <h1 class="text-4xl font-bold text-center text-blue-600 underline decoration-wavy">Hello, WordPress with Tailwind!</h1>
-    <p class="text-center mt-4">Edit this file in <code>wp-content/themes/my-theme/index.php</code></p>
+    <h1 class="text-4xl font-bold text-center text-blue-600 underline decoration-wavy">Monkiria, WordPress with Tailwind!</h1>
+    <p class="text-blue-400 text-center mt-4">Edit this file in <code>wp-content/themes/my-theme/index.php</code></p>
 </main>
 
 <?php get_footer(); ?>
